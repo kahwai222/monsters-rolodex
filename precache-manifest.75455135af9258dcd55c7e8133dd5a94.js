2fc92a8f0c15e31551f1a5b4482e493146f6c837
Kah Wai LIM <kwlim@ntu.edu.sg>
2019-07-16 23:25:36 +0800

    Updates

---
+++ precache-manifest.75455135af9258dcd55c7e8133dd5a94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d759ebc17c466a37762b07edb41a3e4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "36a2ec76e107096e1b71",
+    "url": "/monsters-rolodex/static/css/main.95b906ce.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "36a2ec76e107096e1b71",
+    "url": "/monsters-rolodex/static/js/main.5b84090a.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
